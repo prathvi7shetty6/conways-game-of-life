6cb4205a9764cbd84d69649608098a2eba653382
Natasha Shirke <52173047+natasha-git@users.noreply.github.com>
2024-11-30 14:44:44 -0800

    added header file utils.h

--- src/mpi/main.c
+++ src/mpi/main.c
@@ -1,47 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
-#include <string.h>
-#include <math.h>
-#include <mpi.h>
-#include <unistd.h>
-
-typedef struct GridSection {
-    int rows;
-    int cols;
-    int scale;
-} GridSection;
-
-typedef struct NeighborRanks {
-    int north;
-    int south;
-    int west;
-    int east;
-    int northwest;
-    int northeast;
-    int southwest;
-    int southeast;
-} NeighborRanks;
-
-typedef struct CmdArguments {
-    int generation;
-    int grid_size;
-    char * input_file;
-    char * output_file;
-} CmdArguments;
+#include "utils.h"
 
 CmdArguments program_arguments;
-
-int update_grid_cell(int row, int col, int alive_neighbors, char** current, char** next);
-int count_alive_cells(int row, int col, char** current);
-int handle_grid_edges(GridSection* grid_section, char** current, char** next);
-void determine_neighbors(MPI_Comm communicator, NeighborRanks* neighbors);
-char** allocate_grid(int rows, int cols);
-char** load_file(const char* filepath, int rank, int num_processes, GridSection* grid_section);
-void save_file(const char* output_filepath, int rank, int total_processes, int grid_size, GridSection* grid_section, char** grid_data);
-void execute_game_of_life(char** current_grid, int rank, int num_processes, MPI_Comm communicator, NeighborRanks* neighbors, GridSection* grid_section, CmdArguments* arguments);
-void processArguments(int argc, char** argv, CmdArguments* args);
 
 void processArguments(int argc, char** argv, CmdArguments* args) {
     int opt;
